86c0bf65977a6e5a4cd687db235f6723b96bd12e
Carl Baccus <cjbaccus@gmail.com>
2016-10-24 04:36:36 -0700

    adding recon.sh

---
+++ Recon.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/bash
+
+echo "-- Invoking Recon Daemon --"
+echo 
+echo -n "Type First Name of recon-ee: "
+read fname
+#Check for no response
+if [ -z $fname ]; then
+	echo
+	echo "You did not enter a value"
+	exit
+fi
+echo
+echo -n "Type Last Name of recon-ee: "
+read lname
+if [ -z $lname ];then
+	echo
+	echo "You didn't enter Last Name value"
+	exit
+fi
+echo "Good Job. Starting on $fname $lname."
+echo
+read -p "Press <enter> to continue."
+
+#firefox http://www.411.com/name/$fname-$lname/ &
+firefox http://www.cvgadget.com/person/$fname/$lname &
