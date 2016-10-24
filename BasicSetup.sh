fcbffec307b0295935c45ef1c7a954659f7cad1f
Carl Baccus <cjbaccus@gmail.com>
2016-10-23 21:36:40 -0700

    added apt-cache s > list.txt and install mtr

--- BasicSetup.sh
+++ BasicSetup.sh
@@ -7,7 +7,6 @@ clear
 user=$(whoami)
 break="================================================="
 
-
 echo
 echo "Invoking Daemon..."
 echo
@@ -20,4 +19,14 @@ read fname
 echo -n "Last Name: "
 read lname
 #echo $fname $lname
-firefox http://www.411.com/name/$fname-$lname/
+#firefox http://www.411.com/name/$fname-$lname/
+apt-cache search a > list.txt
+cat list.txt | grep net
+apt-get install mtr -y
+echo $break
+ifconfig | grep inet 
+echo $break
+echo
+echo "All Done " $fname $lname"!!"
+
+
