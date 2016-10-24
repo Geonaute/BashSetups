ece636017df5e585edd08d9bcb8a5c59016de829
Carl Baccus <cjbaccus@gmail.com>
2016-10-23 21:45:31 -0700

    added ufw

--- BasicSetup.sh
+++ BasicSetup.sh
@@ -14,19 +14,29 @@ echo $break
 echo
 #apt-get update
 echo $break
-echo -n "First Name: "
-read fname
-echo -n "Last Name: "
-read lname
+#echo -n "First Name: "
+#read fname
+#echo -n "Last Name: "
+#read lname
 #echo $fname $lname
 #firefox http://www.411.com/name/$fname-$lname/
 apt-cache search a > list.txt
 cat list.txt | grep net
-apt-get install mtr -y
 echo $break
 ifconfig | grep inet 
 echo $break
-echo
-echo "All Done " $fname $lname"!!"
-
+apt-get install mtr -y
+echo "MTR installed"
+echo $break
+apt-get install ufw -y
+echo "UFW installed"
+echo $break
+ufw allow ssh 
+ufw allow https
+ufw allow http
+ufw allow ftp
+ufw enable
+echo "UFW enabled"
+echo $break
+ufw status numbered
 
