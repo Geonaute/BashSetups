a7dd9b6ceab8ff889942a9ac13f1ffdde95818b6
cjbaccus <cjbaccus@gmail.com>
2016-11-23 07:33:14 -0800

    Added in call to generate xlsx report and backkup to report directory

--- CDRdirClean.sh
+++ CDRdirClean.sh
@@ -11,7 +11,7 @@ echo "Starting cleanup..."
 echo 
 echo break
 sleep2
-#rm cmr*
+rm cmr*
 echo 
 echo "All CMRs cleaned out"
 echo 
@@ -19,12 +19,22 @@ python CombineCSV.py
 echo
 echo "Complete!"
 echo 
-echo -n "Type Month of the records for archive: "
-read archiveName
+#echo -n "Type Month of the records for archive: "
+#read archiveName
+DYEAR="$(date | cut -d " " -f6)"
+DMONTH="$(date | cut -d " " -f2)"
+DDAY="$(date | cut -d " " -f3)"
+archiveName=${DMONTH}${DYEAR}
+echo
 echo $archiveName
-tar cvzf $archiveName.tar.gz cdr*
+tar cvzf $archiveName.raw.tar.gz cdr*
 tar cvzf $archiveName.output.tar.gz output.csv
-mv $archiveName.tar.gz Archive/
+mv $archiveName.raw.tar.gz Archive/RAW/
+mv $archiveName.output.tar.gz Archive/
 echo "MOVED"
 echo
 #rm cdr*
+python PyCDR.py output.csv $archiveName.helpdesk
+echo "Report Created"
+mv $archiveName.helpdesk.xlsx Reports/
+echo "Report backed up"
